dd465da71d18fe6030f4834ee5081efa15ec1e41
Artem Rymarchuk <a.rymarchuk@a-tron.de>
2026-01-06 12:05:54 +0100

    clean-up

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,43 +1,50 @@
 import Image from 'next/image'
 
-export default function Header () {
+export default function Header() {
     return (
         <header className='flex flex-col items-center justify-center h-dvh w-full bg-[url(/AtronHeader.png)] bg-no-repeat bg-cover bg-bottom-right'>
-            {/* <div className='relative w-full h-full bg-gray-500'>
-                <Image
-                    src={headerImg}
-                    alt='header-background-image'
-                    fill
-                    className='object-cover object-bottom-right'
-                    priority
-                />
-            </div> */}
             <div className='flex flex-col w-1/2 h-full items-center justify-between pb-44 pt-14 z-10'>
                 <div className='flex items-ceter justify-between w-full cursor-pointer'>
-                    <Image 
+                    <Image
                         src="/A-tron-logo.avif"
                         alt='A-TRON'
                         width={200}
                         height={40}
                         priority
                     />
-                    <div className='flex items-center justify-center'>
-                        <a className='relative mr-6 cursor-pointer before:content-["+1_234_555-55-55"] before:opacity-0 hover:before:opacity-100 before:absolute before:right-full before:top-1/2 before:-translate-y-1/2 before:w-max before:pr-3 before:font-bold before:text-xl before:text-white before:uppercase before:transition-opacity duration-300 ease-in-out'>
-                            <Image 
-                                src="./call.svg"
-                                alt='call us'
-                                width={30}
-                                height={30}
-                            />
-                        </a>
-                        <a className='bg-[url(/menu.svg)] transition-bg duration-200 ease-in-out hover:bg-[url(/menu-hover.svg)] bg-no-repeat bg-cover rotate-180 h-6 w-9 cursor-pointer'>
-                            {/* <Image 
-                                src="./menu.svg"
-                                alt='open menu'
-                                width={30}
-                                height={20}
-                            /> */}
-                        </a>
+                    <div className='flex items-start justify-center'>
+                        <div className='flex items-center'>
+                            <a className='
+                            relative
+                            mr-6
+                            cursor-pointer
+                            before:content-["+1_234_555-55-55"]
+                            before:opacity-0
+                            hover:before:opacity-100
+                            before:absolute
+                            before:right-full before:top-1/2 before:-translate-y-1/2
+                            before:w-max
+                            before:pr-3
+                            before:font-bold before:text-xl before:text-white before:uppercase
+                            before:transition-opacity duration-300 ease-in-out
+                        '>
+                                <Image
+                                    src="./call.svg"
+                                    alt='call us'
+                                    width={30}
+                                    height={30}
+                                />
+                            </a>
+                            <a className='
+                            bg-[url(/menu.svg)]
+                            transition-bg duration-200 ease-in-out
+                            hover:bg-[url(/menu-hover.svg)]
+                            bg-no-repeat bg-cover
+                            rotate-180 h-6 w-9
+                            cursor-pointer
+                        '>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <h1 className='font-bold text-8xl text-white'>Effizienz in jedem Detail</h1>
